1fcf895cd7565fc0584d94360bf0ddd1f5bd6241
Pavel Skobelev <pskobelev@me.com>
2024-12-17 23:24:49 +0300

    format logger

--- app/core/utils.py
+++ app/core/utils.py
@@ -1,12 +1,12 @@
 import logging
 
 
-def configure_logging(
-        level=logging.INFO):
+def configure_logging(level=logging.DEBUG):
     logging.basicConfig(
         # format="%(asctime)s [%(name)s:%(lineno)10d] [%(levelname)-7s] > %(message)s"
         # format='%(asctime)s [%(levelname)-7s] - %(name)s > %(message)s'
-        format='%(asctime)s [%(levelname)-7s] - [%(filename)s).%(funcName)s(%(lineno)d)] > %(message)s'
+        datefmt='%Y-%m-%d %H:%M:%S',
+        format="[%(asctime)s] %(module)s.%(funcName)s:%(lineno)d %(levelname)-7s>> %(message)s"
     )
     logger = logging.getLogger()
     logger.setLevel(level=level)
