ee0fcc6dbf0dc029a36bb8ba6198520ccc7f7aed
Pavel Skobelev <pskobelev@me.com>
2024-12-18 22:23:37 +0300

    https://yapd.atlassian.net/browse/EB-30

--- app/bot/utils.py
+++ app/bot/utils.py
@@ -1,4 +1,6 @@
 import json
+import re
+from datetime import datetime
 
 
 async def format_text(msg):
@@ -18,3 +20,21 @@ async def handle_response(resp):
         raise ValueError(
             f"Server responded with an error: {resp.status}, {error_text}"
         )
+
+
+async def extract_datetime(text):
+    # Шаблон для поиска даты и времени в формате YYYY-MM-DD HH:MM или только даты YYYY-MM-DD
+    pattern = r"\b(\d{4}-\d{2}-\d{2})(?: (\d{2}:\d{2}))?\b"
+    match = re.search(pattern, text)
+    if match:
+        date_part = match.group(1)  # Всегда есть дата
+        time_part = (
+                match.group(2) or "10:30"
+        )  # Подставляем стандартное время, если его нет
+        datetime_str = f"{date_part} {time_part}"
+        try:
+            return datetime.strptime(datetime_str, "%Y-%m-%d %H:%M")
+        except ValueError:
+            raise ValueError("Неверный формат даты")
+    else:
+        return None
