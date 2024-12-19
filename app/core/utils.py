df09ad6b839dd566bcafb90bbce65cdcbe95dcb1
Pavel Skobelev <pskobelev@me.com>
2024-12-19 22:20:52 +0300

    update logger

--- app/core/utils.py
+++ app/core/utils.py
@@ -3,10 +3,8 @@ import logging
 
 def configure_logging(level=logging.DEBUG):
     logging.basicConfig(
-        # format="%(asctime)s [%(name)s:%(lineno)10d] [%(levelname)-7s] > %(message)s"
-        # format='%(asctime)s [%(levelname)-7s] - %(name)s > %(message)s'
-        datefmt='%Y-%m-%d %H:%M:%S',
-        format="[%(asctime)s] %(module)s.%(funcName)s:%(lineno)d %(levelname)-7s>> %(message)s"
+        # datefmt='%Y-%m-%d %H:%M:%S',
+        format="[%(asctime)s]%(levelname)6s | %(module)s:%(lineno)d > %(message)s"
     )
     logger = logging.getLogger()
     logger.setLevel(level=level)
