3804949ff30cc2737ea991227d83eb1b522b84d6
Pavel Skobelev <pskobelev@me.com>
2024-12-20 10:49:44 +0300

    apply migration

---
+++ app/migration/versions/2024_12_19_2302-806d5e4d3be5_update_tables.py
@@ -0,0 +1,82 @@
+"""update tables
+
+Revision ID: 806d5e4d3be5
+Revises:
+Create Date: 2024-12-19 23:02:52.413667
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision: str = "806d5e4d3be5"
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "events",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("chat_id", sa.Integer(), nullable=True),
+        sa.Column("event_name", sa.String(), nullable=True),
+        sa.Column("active", sa.Boolean(), nullable=False),
+        sa.Column(
+            "created_at",
+            sa.DateTime(timezone=True),
+            server_default=sa.text("CURRENT_TIMESTAMP"),
+            nullable=False,
+        ),
+        sa.Column("updated_at", sa.DateTime(timezone=True), nullable=False),
+        sa.PrimaryKeyConstraint("id", name=op.f("pk_events")),
+    )
+    op.create_table(
+        "users",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("telegram_id", sa.BigInteger(), nullable=False),
+        sa.Column("name", sa.String(), nullable=False),
+        sa.Column("username", sa.String(), nullable=True),
+        sa.Column(
+            "created_at",
+            sa.DateTime(timezone=True),
+            server_default=sa.text("CURRENT_TIMESTAMP"),
+            nullable=False,
+        ),
+        sa.Column("updated_at", sa.DateTime(timezone=True), nullable=False),
+        sa.PrimaryKeyConstraint("id", name=op.f("pk_users")),
+        sa.UniqueConstraint("telegram_id", name=op.f("uq_users_telegram_id")),
+    )
+    op.create_table(
+        "userevents",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("user_id", sa.BigInteger(), nullable=False),
+        sa.Column("event_id", sa.Integer(), nullable=False),
+        sa.Column(
+            "created_at",
+            sa.DateTime(timezone=True),
+            server_default=sa.text("CURRENT_TIMESTAMP"),
+            nullable=False,
+        ),
+        sa.Column("updated_at", sa.DateTime(timezone=True), nullable=False),
+        sa.ForeignKeyConstraint(
+            ["event_id"],
+            ["events.id"],
+            name=op.f("fk_userevents_event_id_events"),
+        ),
+        sa.ForeignKeyConstraint(
+            ["user_id"],
+            ["users.telegram_id"],
+            name=op.f("fk_userevents_user_id_users"),
+        ),
+        sa.PrimaryKeyConstraint("id", name=op.f("pk_userevents")),
+    )
+
+
+def downgrade() -> None:
+    op.drop_table("userevents")
+    op.drop_table("users")
+    op.drop_table("events")
