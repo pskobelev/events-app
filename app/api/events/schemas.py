05e6ae906bfb923b79c7453819452e45ed2af47c
Pavel Skobelev <pskobelev@me.com>
2024-12-24 13:26:35 +0300

    add date field validator

--- app/api/events/schemas.py
+++ app/api/events/schemas.py
@@ -1,14 +1,25 @@
-from pydantic import BaseModel, ConfigDict
+from datetime import datetime
+
+from pydantic import BaseModel, ConfigDict, field_validator
 
 
 class EventBase(BaseModel):
     event_name: str
     chat_id: int
+    event_date: datetime
+
+    @field_validator("event_date", mode="before")
+    def parse_event_date(cls, value):
+        if isinstance(value, str):
+            # Преобразуем строку формата "YYYY-MM-DD HH:MM" в datetime
+            return datetime.strptime(value, "%Y-%m-%d %H:%M")
+        return value
+
     model_config = ConfigDict(from_attributes=True)
 
 
 class CreateEvent(EventBase):
-    event_name: str
+    pass
 
 
 class UserEventCreate(BaseModel):
