9c053424f94dee6d86c6594710096ef15b2106a3
Pavel Skobelev <pskobelev@me.com>
2025-01-22 22:21:32 +0300

    update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -2,22 +2,21 @@ import logging
 
 from aiogram import Bot, Dispatcher
 from aiogram.exceptions import TelegramNetworkError
-from bot.handlers import user_router
+from handlers import user_router
 
 from core.config import settings
 
 logging.basicConfig(level=logging.INFO, format=settings.logging.log_format)
-
 logger = logging.getLogger(__name__)
+
 bot = Bot(token=settings.bot.token)
 dp = Dispatcher()
 dp.include_router(user_router)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     try:
         dp.run_polling(bot)
-        logger.info("Bot is started!")
-
+        logger.info("I'm alive!")
     except TelegramNetworkError:
         logger.exception(
             "Network Error",
