a9090274bc75d84b2f0a82c83b0160e81c56332a
Pavel Skobelev <pskobelev@me.com>
2025-01-28 23:32:34 +0300

    add keyb.py

--- bot/keyb.py
+++ bot/keyb.py
@@ -1,11 +1,35 @@
+from aiogram.types import (
+    BotCommand,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+)
+
 BUTTONS = [
     {"text": "В игре ✅", "choice": "in_game"},
     {"text": "Пас ⛔", "choice": "pass"},
     {"text": "Думаю 🫠", "choice": "thinking"},
-    {"text": "foo", "choice": "bar"},
 ]
 
-MENU = [{
-    "start": "tbd",
-    "help":  "Show all commands",
-}]
+MENU_CMD = [
+    BotCommand(command="new_game", description="Start a new game"),
+    BotCommand(command="close", description="Finish active game"),
+    BotCommand(command="foo", description="Bar"),
+    BotCommand(command="list", description="List games"),
+]
+
+
+async def set_user_choice_kb(event_id):
+    buttons = [
+        InlineKeyboardButton(
+            text="Играю ⚽", callback_data=f"uc_play:{event_id}"
+        ),
+        InlineKeyboardButton(
+            text="Подумаю 🤔", callback_data=f"uc_maybe:{event_id}"
+        ),
+        InlineKeyboardButton(
+            text="Не могу 🙅‍♂️", callback_data=f"uc_cannot:{event_id}"
+        ),
+    ]
+    return InlineKeyboardMarkup(
+        row_width=3, inline_keyboard=[buttons], resize_keyboard=True
+    )
